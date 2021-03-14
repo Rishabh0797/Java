4aab43ce19699cadb4321c55887a3b2fd15a6131
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2021-03-14 23:38:34 +0530

    Create argsdemo.java

---
+++ Command Line Argument/argsdemo.java
@@ -0,0 +1,7 @@
+ class argsdemo
+{
+	public static void main(String[] args) {
+	System.out.println("Hello "+args[0]+" ,your age is "+args[1]);    //Enter Your name and Age in command Line Argument
+		
+	}
+}
