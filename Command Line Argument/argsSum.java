0ef927e1bd5b19131099e5f748ca3cd6ef823a96
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2021-03-14 23:43:05 +0530

    Create argsSum.java

---
+++ Command Line Argument/argsSum.java
@@ -0,0 +1,14 @@
+class argsSum
+{
+	public static void main(String[] args) {
+	     
+	     //Enter two number in Command line arguments
+	     
+	    int a=Integer.parseInt(args[0]);
+	    int b=Integer.parseInt(args[1]);
+	    System.out.print("Sum is : ");
+	    System.out.println(a+b);
+	    
+		
+	}
+}
